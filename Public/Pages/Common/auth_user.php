fa9ef950eebcceb9902ae1a1b3c6d22da23dde0f
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-05-08 14:39:27 +0530

    Update auth_user.php

--- Public/Pages/Common/auth_user.php
+++ Public/Pages/Common/auth_user.php
@@ -6,8 +6,8 @@ if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 // For example, assuming you have a $userRole variable set somewhere based on user data
 $userRole =$_SESSION['role'];
 $allowedRoles = ['User', 'Manager', 'Agent', 'Admin', 'Supervisor']; // Define allowed roles
-
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != 1 || !isset($userRole) || !in_array($userRole, $allowedRoles)) {
+// || !isset($userRole) || !in_array($userRole, $allowedRoles)
+if (!isset($_SESSION['loggedin']) ) {
     // The user is not logged in or does not have an allowed role
     header('Location: ../index.php/Login_to_CustCount'); // Redirect to the login page
     exit; // Prevent further script execution after redirect
